bf833acf6aead173326fddd8e93c29a0618062d6
ibdkn <ibudiakina@gmail.com>
2023-11-10 13:45:43 +0300

    added the task-6

---
+++ task-6/script.js
@@ -0,0 +1,9 @@
+// Create a class Ghost
+//
+// Ghost objects are instantiated without any arguments.
+//
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+var Ghost = function() {
+    // your code goes here
+};
