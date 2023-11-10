4019c0c4ca7f8ed0c2cca9cef3b3eaa8304a7594
ibdkn <ibudiakina@gmail.com>
2023-11-10 13:33:27 +0300

    added text of task in js file

--- task-2/script.js
+++ task-2/script.js
@@ -1,3 +1,5 @@
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final
+// outcome must be sorted and not have any duplicate.
 function mergeArrays(a, b) {
     let newArray = a.concat(b);
     let result = newArray.filter((number, index) => {
