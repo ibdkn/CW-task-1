53c530655d5b06ff84aebc09fa94965b550edbde
ibdkn <ibudiakina@gmail.com>
2023-11-23 17:06:07 +0300

    completed whoLikesIt.js

--- 6kyu/whoLikesIt.js
+++ 6kyu/whoLikesIt.js
@@ -11,10 +11,13 @@
 
 // SOLUTION
 function likes(names) {
-    if(names && names.length > 0) {
-        return 1;
-    } else {
-        return "no one likes this";
+    names.length === 0 && (names = ["no one"]);
+    let [a, b, c, ...others] = names;
+    switch (names.length) {
+        case 1: return `${a} likes this`;
+        case 2: return `${a} and ${b} like this`;
+        case 3: return `${a}, ${b} and ${c} like this`;
+        default: return `${a}, ${b} and ${others.length + 1} others like this`;
     }
 }
 
