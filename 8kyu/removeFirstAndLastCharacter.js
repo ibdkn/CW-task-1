dede157e69272cd97340ada2c2b6c31d2d74a24f
ibdkn <ibudiakina@gmail.com>
2023-11-23 17:22:52 +0300

    added removeFirstAndLastCharacter.js

---
+++ 8kyu/removeFirstAndLastCharacter.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a
+// string. You're given one parameter, the original string. You don't have to worry with strings with less than two
+// characters.
+
+// SOLUTION
+function removeChar(str) {
+    let array = str.split('');
+    array.shift();
+    array.pop();
+    return array.join('');
+
+    // short solution
+    // return str.slice(1, -1);
+}
+
+console.log(removeChar('eloquent'));
