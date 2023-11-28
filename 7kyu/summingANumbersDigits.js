dce36165a80e6fa5b5372eeb88a81e2bbc6d78c1
ibdkn <ibudiakina@gmail.com>
2023-11-28 20:09:42 +0300

    added summingANumbersDigits.js

---
+++ 7kyu/summingANumbersDigits.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+// For example: (Input --> Output)
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+
+// SOLUTION
