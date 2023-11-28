f40982c024ac895962ee854b6188623277d83db4
ibdkn <ibudiakina@gmail.com>
2023-11-28 20:27:09 +0300

    completed sortTheOdd.js

--- 6kyu/sortTheOdd.js
+++ 6kyu/sortTheOdd.js
@@ -7,3 +7,9 @@
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
 // SOLUTION
+function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+}
+
+console.log(sortArray([1, 3, 2, 8, 5, 4]));
