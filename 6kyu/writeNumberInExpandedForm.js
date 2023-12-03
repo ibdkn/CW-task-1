ab1ea94553e43d40d5f00949c214958536c41e5d
ibdkn <ibudiakina@gmail.com>
2023-12-03 21:45:34 +0300

    completed writeNumberInExpandedForm.js

--- 6kyu/writeNumberInExpandedForm.js
+++ 6kyu/writeNumberInExpandedForm.js
@@ -8,7 +8,13 @@
 
 // SOLUTION
 function expandedForm(num) {
-
+    return num.toString()
+        .split("")
+        .reverse()
+        .map((a, i) => a * Math.pow(10, i))
+        .filter(a => a > 0)
+        .reverse()
+        .join(" + ");
 }
 
 console.log(expandedForm(70304));
