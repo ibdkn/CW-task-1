609147db01edd18d66e78847f444f7caa7c00d13
ibdkn <ibudiakina@gmail.com>
2024-03-29 23:59:21 +0300

    completed alternateCapitalization.js

--- 7kyu/alternateCapitalization.js
+++ 7kyu/alternateCapitalization.js
@@ -9,7 +9,7 @@
 
 function capitalize(s) {
     const even = s.split('').map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join('');
-    const odd = s.split('').map((l, i) => i % 2 === 0 ? l.toLowerCase() : l).join('');
+    const odd = s.split('').map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join('');
 
     return [even, odd];
 }
