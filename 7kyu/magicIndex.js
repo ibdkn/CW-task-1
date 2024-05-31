86bf315e00c65b1e328b3c8951ba674fa7337a89
ibdkn <ibudiakina@gmail.com>
2024-05-31 11:57:03 +0300

    completed magicIndex.js

---
+++ 7kyu/magicIndex.js
@@ -0,0 +1,16 @@
+// A magic index in an array A[1...n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+//
+// findMagic([-20,-10,2,10,20]); // Returns 2
+
+
+function findMagic(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === i) {
+            return arr[i]
+        }
+    }
+    return -1
+}
+
+console.log(findMagic([-20,-10,2,10,20])) // 2
+console.log(findMagic([6, 5, 83, 5, 3, 18])) // -1
