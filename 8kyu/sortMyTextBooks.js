a2d36307552fa56ffa47c27dac03c533d74294be
ibdkn <ibudiakina@gmail.com>
2024-06-06 23:12:28 +0300

    completed sortMyTextBooks.js

---
+++ 8kyu/sortMyTextBooks.js
@@ -0,0 +1,15 @@
+// HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+//
+// The sorting should NOT be case sensitive
+
+function sorter(textbooks) {
+    return textbooks.sort((a, b) => {
+        const lowerA = a.toLowerCase();
+        const lowerB = b.toLowerCase();
+        if (lowerA < lowerB) return -1;
+        if (lowerA > lowerB) return 1;
+        return 0;
+    });
+}
+
+console.log(sorter(['Algebra', 'History', 'Geometry', 'English'])); // [ 'Algebra', 'English', 'Geometry', 'History' ]
